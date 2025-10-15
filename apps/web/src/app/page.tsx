7bdba9fa681b89334a6fc7a75efa5491bcaeb543
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-15 10:12:41 +0100

    .

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,11 @@ export default async function Home() {
   await prisma.user.create({
     data: {},
   });
-  const x = await prisma.user.findMany();
+  const x = await prisma.user.findMany({
+    where: {
+      deletedAt: null,
+    },
+  });
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
