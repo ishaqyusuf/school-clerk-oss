0ca0f65dc95471d3fd377c8bcca5b81d4f260406
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-11-08 22:28:28 +0100

    wip

--- apps/dashboard/src/app/dashboard/[domain]/(temp)/login2/page.tsx
+++ apps/dashboard/src/app/dashboard/[domain]/(temp)/login2/page.tsx
@@ -1,3 +1,9 @@
+import { constructMetadata } from "@school-clerk/utils/construct-metadata";
+export async function generateMetadata() {
+  return constructMetadata({
+    title: `Login - GND Storefront`,
+  });
+}
 export default async function Login() {
   return <>LOGIN @@@@</>;
 }
